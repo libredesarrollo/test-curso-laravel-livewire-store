e90e3e919a71734f49640bb1478815fbf3d41bbd
libredesarrollo <desarrollolibre.net@gmail.com>
2022-03-22 15:39:42 -0400

    varios

--- app/Http/Livewire/Dashboard/Category/Save.php
+++ app/Http/Livewire/Dashboard/Category/Save.php
@@ -13,7 +13,32 @@ class Save extends Component
 
     public $image;
 
-    protected $rules =[
+    public $category;
+
+    public function mount($id = null)
+    {
+        $this->init($id);
+    }
+
+    private function init($id)
+    {
+        $category = null;
+        if ($id) {
+            $category = Category::findOrFail($id);
+        }
+
+        $this->category = $category;
+
+        //dd( $this->category);
+
+        if ($this->category) {
+            $this->title = $this->category->title;
+            $this->text = $this->category->body;
+            //$this->slug = $this->category->slug;
+        }
+    }
+
+    protected $rules = [
         'title' => 'required|min:2|max:500',
         //'slug' => 'max:500',
         'text' => 'nullable'
@@ -29,10 +54,18 @@ class Save extends Component
 
         $this->validate();
 
-        Category::create([
-            'title' => $this->title,
-            'slug' => str($this->title)->slug(),
-            'body' => $this->text,
-        ]);
+        if ($this->category) {
+            $this->category->update(
+                [
+                    'title' => $this->title,
+                    'body' => $this->text,
+                ]
+            );
+        } else
+            Category::create([
+                'title' => $this->title,
+                'slug' => str($this->title)->slug(),
+                'body' => $this->text,
+            ]);
     }
 }
